e534c92a6ff67186a6df54924b7783a520f6e9b0
包星星 <743697070@qq.com>
2019-07-01 16:58:12 +0800

    废弃demo删除

--- demo/Index/store/index.js
+++
@@ -1,7 +0,0 @@
-import demoMenuStore from './MenuStore';
-import FeaturesStore from './FeaturesStore';
-
-export default {
-    demoMenuStore,
-    FeaturesStore
-};
--- demo/index.js
+++
@@ -1,6 +0,0 @@
-import React, { lazy } from 'react';
-import PrivateRoute from 'src/components/PrivateRoute';
-
-const Demo = lazy(() => import('demo/Index/index'));
-
-export default <PrivateRoute key="Demo" path="/demo-page" component={Demo} />;
