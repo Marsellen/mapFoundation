ac40e72d7ffc8bb65692306209eac7716fff1a24
包星星 <743697070@qq.com>
2019-08-09 16:33:22 +0800

    交通灯关联关系操作按钮

--- src/config/DataLayerConfig.js
+++ src/config/DataLayerConfig.js
@@ -93,15 +93,8 @@ export const DATA_LAYER_MAP = {
         label: '交通信号灯',
         id: 'LIGHT_ID',
         spec: 'AD_TrafficLight',
-        tools: ['ADD_FACADEREC_TANGLE'],
-        rightTools: [
-            'delete',
-            'insertPoints',
-            'changePoints',
-            'deletePoints',
-            'ADD_REL',
-            'DEL_REL'
-        ]
+        tools: ['ADD_FACADEREC_TANGLE', 'ADD_REL', 'DEL_REL'],
+        rightTools: ['delete', 'insertPoints', 'changePoints', 'deletePoints']
     },
     AD_Pole: {
         label: '杆状物',
