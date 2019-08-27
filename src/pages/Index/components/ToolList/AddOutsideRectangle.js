46c40fa867925d29e2f9fb2a25860ba799a6553a
cuijie <jie2.cui@ecarx.com.cn>
2019-08-27 16:57:43 +0800

    工具提示弹窗

--- src/pages/Index/components/ToolList/AddOutsideRectangle.js
+++ src/pages/Index/components/ToolList/AddOutsideRectangle.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import ToolIcon from 'src/components/ToolIcon';
 import { inject, observer } from 'mobx-react';
+import AdMessage from 'src/components/AdMessage';
 
 @inject('DataLayerStore')
 @observer
 class AddOutsideRectangle extends React.Component {
     render() {
+        const { DataLayerStore } = this.props;
+        let visible = DataLayerStore.editType == 'new_vertical_matrix';
         return (
-            <ToolIcon
-                icon="renyiwaijiejuxing"
-                title="任意外接立面矩形"
-                action={this.action}
-            />
+            <span>
+                <ToolIcon
+                    icon="renyiwaijiejuxing"
+                    title="任意外接立面矩形"
+                    action={this.action}
+                />
+                <AdMessage visible={visible} content={this.content()} />
+            </span>
         );
     }
 
     action = () => {
         const { DataLayerStore } = this.props;
+        if (DataLayerStore.editType == 'new_vertical_matrix') return;
         DataLayerStore.newVerticalMatrix();
+    };
+
+    content = () => {
+        return <label>添加外接立面矩形</label>;
     };
 }
 
--- src/pages/Index/components/ToolList/Ranging.js
+++ src/pages/Index/components/ToolList/Ranging.js
@@ -2,27 +2,37 @@
 import React from 'react';
 import ToolIcon from 'src/components/ToolIcon';
 import { inject, observer } from 'mobx-react';
+import AdMessage from 'src/components/AdMessage';
 
 @inject('DataLayerStore')
 @inject('taskStore')
 @observer
 class Ranging extends React.Component {
     render() {
-        const { taskStore } = this.props;
+        const { taskStore, DataLayerStore } = this.props;
         const { activeTaskId } = taskStore;
+        let visible = DataLayerStore.editType == 'meature_distance';
         return (
-            <ToolIcon
-                icon="ceju"
-                title="测距"
-                action={this.action}
-                disabled={!activeTaskId}
-            />
+            <span>
+                <ToolIcon
+                    icon="ceju"
+                    title="测距"
+                    action={this.action}
+                    disabled={!activeTaskId}
+                />
+                <AdMessage visible={visible} content={this.content()} />
+            </span>
         );
     }
 
     action = () => {
         const { DataLayerStore } = this.props;
+        if (DataLayerStore.editType == 'meature_distance') return;
         DataLayerStore.startMeatureDistance();
+    };
+
+    content = () => {
+        return <label>请选择一个点开始测距</label>;
     };
 }
 
