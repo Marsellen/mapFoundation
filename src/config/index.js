77ed320f7534d2d4ac28155f8d9726f8d774da26
包星星 <743697070@qq.com>
2019-10-08 10:46:14 +0800

    版本号更新

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export default {
     title: 'Ecarx React App',
-    version: 'v1.0.6.0-sprint6',
+    version: 'v1.0.6.2-sprint6-rc2',
 
     urlConfig: {
         point_clouds: '/point_clouds/cloud.js',
