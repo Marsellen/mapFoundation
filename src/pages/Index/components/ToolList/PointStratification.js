20c142aaec5c040352a98688226447d73cde2f41
vivian <wei.lu@ecarx.com.cn>
2019-10-15 19:05:04 +0800

    DAA3DEP-334 点云分高程显示

--- src/pages/Index/components/ToolList/PointStratification.js
+++ src/pages/Index/components/ToolList/PointStratification.js
@@ -1,113 +1,94 @@
 import React from 'react';
-import { Popover, Tooltip, Slider } from 'antd';
+import { Popover, Slider } from 'antd';
 import { inject, observer } from 'mobx-react';
 import IconFont from 'src/components/IconFont';
+import 'src/assets/less/components/tool-icon.less';
 
+@inject('DataLayerStore')
 @inject('TaskStore')
 @observer
 class PointStratification extends React.Component {
     state = {
-        clicked: false,
-        hovered: false,
-        updateKey: 0
+        popoverVisible: false,
+        sliderMinValue: 0,
+        sliderMaxValue: 100,
+        sliderValue: null
     };
 
-    hide = () => {
-        this.setState({
-            clicked: false,
-            hovered: false
-        });
-    };
-
-    handleHoverChange = visible => {
-        if (!this.state.clicked) {
+    handlePopoverChange = visible => {
+        if (visible) {
+            const { DataLayerStore } = this.props;
+            const { pointCloudLayerHeightRange } = DataLayerStore;
+            const [
+                pointCloudLayerMinHeight,
+                pointCloudLayerMaxHeight
+            ] = pointCloudLayerHeightRange;
+            let { sliderValue, sliderMinValue } = this.state;
+            if (sliderValue || sliderMinValue !== pointCloudLayerMinHeight) {
+                sliderValue = pointCloudLayerHeightRange;
+            }
             this.setState({
-                hovered: visible
+                popoverVisible: true,
+                sliderMinValue: pointCloudLayerMinHeight,
+                sliderMaxValue: pointCloudLayerMaxHeight,
+                sliderValue
+            });
+        } else {
+            this.setState({
+                popoverVisible: false
             });
         }
     };
 
-    handleClickChange = visible => {
-        const { TaskStore } = this.props;
-        const { activeTaskId } = TaskStore;
-        if (!activeTaskId) return;
-        this.setState({
-            clicked: visible,
-            hovered: false
-        });
-    };
-    render() {
-        const { TaskStore } = this.props;
-        const { activeTaskId } = TaskStore;
-        return (
-            <Popover
-                content={this._renderContent()}
-                trigger="click"
-                visible={this.state.clicked}
-                onVisibleChange={this.handleClickChange}>
-                <Tooltip
-                    placement="bottom"
-                    title="设置点云分高层"
-                    visible={this.state.hovered}
-                    onVisibleChange={this.handleHoverChange}>
-                    <IconFont
-                        type="icon-dianyunfengaochengxianshi"
-                        className={`ad-icon ${!activeTaskId &&
-                            'ad-disabled-icon'}`}
-                    />
-                </Tooltip>
-            </Popover>
+    handleSlideChange = sliderValue => {
+        this.setState(
+            {
+                sliderValue
+            },
+            () => {
+                pointCloudLayer.setDisplayAltitudeMin(sliderValue[0]);
+                pointCloudLayer.setDisplayAltitudeMax(sliderValue[1]);
+            }
         );
-    }
+    };
 
     _renderContent() {
-        let value = window.pointCloudLayer
-            ? pointCloudLayer.getElevationRange()
-            : 0;
-
+        const { sliderValue, sliderMinValue, sliderMaxValue } = this.state;
         return (
-            <div>
-                <div
-                    style={{
-                        color: '#9e9e9e',
-                        textAlign: 'center'
-                    }}>
-                    {this.formatter(value[1])}
-                </div>
+            <div className="ad-slider-box">
+                <p>{sliderMaxValue}米</p>
                 <Slider
-                    className="flex-1"
-                    range
-                    onChange={this.onChange}
+                    className="ad-slider-vertical"
+                    onChange={this.handleSlideChange}
+                    min={sliderMinValue}
+                    max={sliderMaxValue}
+                    value={sliderValue}
                     step={0.01}
-                    max={value[1]}
-                    min={value[0]}
-                    tipFormatter={this.formatter}
-                    style={{ height: 100 }}
                     vertical
-                    defaultValue={value}
+                    range
                 />
-                <div
-                    style={{
-                        color: '#9e9e9e',
-                        textAlign: 'center'
-                    }}>
-                    {this.formatter(value[0])}
-                </div>
+                <p>{sliderMinValue}米</p>
             </div>
         );
     }
 
-    formatter = value => {
-        return `${parseFloat((value * 1000) / 1000)}米`;
-    };
-
-    onChange = value => {
-        this.setState({
-            updateKey: Math.random()
-        });
-        pointCloudLayer.setDisplayAltitudeMin(value[0]);
-        pointCloudLayer.setDisplayAltitudeMax(value[1]);
-    };
+    render() {
+        const { TaskStore } = this.props;
+        const { activeTaskId } = TaskStore;
+        const { popoverVisible } = this.state;
+        return (
+            <Popover
+                content={this._renderContent()}
+                trigger="click"
+                onVisibleChange={this.handlePopoverChange}
+                visible={popoverVisible}>
+                <IconFont
+                    type="icon-dianyunfengaochengxianshi"
+                    className={`ad-icon ${!activeTaskId && 'ad-disabled-icon'}`}
+                />
+            </Popover>
+        );
+    }
 }
 
 export default PointStratification;
