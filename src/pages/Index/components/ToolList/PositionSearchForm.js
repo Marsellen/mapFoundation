8751ff484a1df383d569d383496a59bdf26d6302
包星星 <743697070@qq.com>
2019-10-18 17:00:58 +0800

    坐标查询问题修改

--- src/pages/Index/components/ToolList/PositionSearchForm.js
+++ src/pages/Index/components/ToolList/PositionSearchForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Input } from 'antd';
+import { Form, InputNumber } from 'antd';
 
 const FormItem = Form.Item;
 
@@ -11,31 +11,20 @@ class PositionSearchForm extends React.Component {
             <Form labelCol={{ span: 6 }} wrapperCol={{ span: 14 }}>
                 <FormItem label="x坐标：">
                     {form.getFieldDecorator('x', {
-                        rules: [{ required: true, message: 'x坐标必填!' }],
-                        getValueFromEvent: e => {
-                            let value = Number(e.target.value);
-                            return !value ? e.target.value : value;
-                        }
-                    })(<Input />)}
+                        rules: [{ required: true, message: 'x坐标必填!' }]
+                    })(<InputNumber className="position-input" />)}
                     <span className="suffix-form-text">m</span>
                 </FormItem>
                 <FormItem label="y坐标：">
                     {form.getFieldDecorator('y', {
-                        rules: [{ required: true, message: 'y坐标必填!' }],
-                        getValueFromEvent: e => {
-                            let value = Number(e.target.value);
-                            return !value ? e.target.value : value;
-                        }
-                    })(<Input />)}
+                        rules: [{ required: true, message: 'y坐标必填!' }]
+                    })(<InputNumber className="position-input" />)}
                     <span className="suffix-form-text">m</span>
                 </FormItem>
                 <FormItem label="z坐标：">
-                    {form.getFieldDecorator('z', {
-                        getValueFromEvent: e => {
-                            let value = Number(e.target.value);
-                            return !value ? e.target.value : value;
-                        }
-                    })(<Input />)}
+                    {form.getFieldDecorator('z', {})(
+                        <InputNumber className="position-input" />
+                    )}
                     <span className="suffix-form-text">m</span>
                 </FormItem>
             </Form>
