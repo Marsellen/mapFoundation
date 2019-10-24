20da69c24062441297c8abc6fdc02194c5dab516
cuijie <jie2.cui@ecarx.com.cn>
2019-10-24 18:32:01 +0800

    属性列表移至左侧，表单校验修改，规格属性调整

--- src/utils/form/validator.js
+++ src/utils/form/validator.js
@@ -18,6 +18,7 @@ const getCharValidator = option => {
         if (value.length > length) {
             callback(new Error(`字符长度应小于${length}`));
         }
+        callback();
     };
 };
 
@@ -26,6 +27,10 @@ const getNumericValidator = option => {
     switch (rule) {
         case 'range':
             return getNumericRangeValidator(params);
+        case 'max':
+            return getNumericMaxValidator(params);
+        case 'maxLength':
+            return getNumericMaxLengthValidator(params);
     }
 };
 
@@ -36,9 +41,55 @@ const getNumericRangeValidator = params => {
     return (rule, value, callback) => {
         let number = Number(value);
         if (number < min || number > max) {
-            callback(new Error(`取值方位应在[${min}, ${max}]`));
+            callback(new Error(`取值范围应在[${min}, ${max}]`));
         }
+        callback();
     };
 };
 
-const getDecimalValidator = option => {};
+const getNumericMaxValidator = params => {
+    let [max] = params;
+    return (rule, value, callback) => {
+        let number = Number(value);
+        if (number > max) {
+            callback(new Error(`最大值不超过${max}`));
+        }
+        callback();
+    };
+};
+
+const getNumericMaxLengthValidator = params => {
+    let [maxLength] = params;
+    return (rule, value, callback) => {
+        if (value.length > maxLength) {
+            callback(new Error(`最大长度应为${maxLength}位数字`));
+        }
+        callback();
+    };
+};
+
+const getDecimalValidator = option => {
+    let [integerLength, floatLength] = option;
+    integerLength = Number(integerLength) - 1;
+    floatLength = Number(floatLength);
+    let reg = new RegExp(
+        '^[1-9]\\d{0,' +
+            integerLength +
+            '}(\\.\\d{1,' +
+            floatLength +
+            '})?$|^0(\\.\\d{1,' +
+            floatLength +
+            '})?$'
+    );
+    return (rule, value, callback) => {
+        if (!reg.test(value)) {
+            callback(
+                new Error(
+                    `整数部分不超过${integerLength +
+                        1}位数字,小数部分不超过${floatLength}位小数`
+                )
+            );
+        }
+        callback();
+    };
+};
