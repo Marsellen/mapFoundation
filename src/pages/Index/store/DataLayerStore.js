4abbc272e62e65fb5c17c62efd1251ce2f8de6dc
cuijie <jie2.cui@ecarx.com.cn>
2019-11-05 15:22:58 +0800

    '俯视图模式禁用部分编辑图层'

--- src/pages/Index/store/DataLayerStore.js
+++ src/pages/Index/store/DataLayerStore.js
@@ -250,6 +250,8 @@ class DataLayerStore extends LayerStore {
     @action topViewMode = opt => {
         if (opt) {
             this.isTopView = true;
+            this.editType = 'normal';
+            this.exitEdit();
         } else {
             this.isTopView = false;
         }
