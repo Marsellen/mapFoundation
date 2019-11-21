f4997ca10b8d5c970c35924d565993b5a2826501
包星星 <743697070@qq.com>
2019-11-21 15:39:26 +0800

    合并后重复提示去除

--- src/pages/Index/components/RightMenuModal.js
+++ src/pages/Index/components/RightMenuModal.js
@@ -479,7 +479,6 @@ class RightMenuModal extends React.Component {
                     OperateHistoryStore.add(history);
                     editLog.store.add(log);
                     AdEmitter.emit('fetchViewAttributeData');
-                    message.success('操作完成', 3);
                 } catch (e) {
                     console.log(e);
                     message.warning('操作失败:' + e.message, 3);
