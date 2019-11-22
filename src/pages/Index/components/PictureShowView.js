f31b58a41d44f688a2a9c1ec64c3796f9b041993
包星星 <743697070@qq.com>
2019-11-22 15:21:35 +0800

    图片组件按钮列表扩展上下轨迹点功能

--- src/pages/Index/components/PictureShowView.js
+++ src/pages/Index/components/PictureShowView.js
@@ -7,42 +7,48 @@ import { completeSecendUrl } from 'src/utils/taskUtils';
 
 @inject('TaskStore')
 @inject('PictureShowStore')
+@inject('DataLayerStore')
 @observer
 class PictureShowView extends React.Component {
-    options = {
-        inline: true, //内联模式
-        // button: false,//右上角全屏
-        navbar: false,
-        // title: false,
-        toolbar: {
-            //工具栏
-            zoomIn: 1,
-            zoomOut: 1,
-            oneToOne: 1,
-            reset: 1,
-            prev: 1,
-            play: 0,
-            next: 1,
-            rotateLeft: 0,
-            rotateRight: 0,
-            flipHorizontal: 0,
-            flipVertical: 0
-        },
-        tooltip: false, //放大或缩小时显示具有图像比率（百分比）的工具提示
-        rotatable: false, //旋转图像
-        fullscreen: true, //全屏
-        transition: false, //过渡
-        backdrop: true, //控制背景
-        minWidth: 600,
-        zoomRatio: 0.1, //通过旋转鼠标缩放图像时定义比率
-        minZoomRatio: 0.1, //最小比例
-        maxZoomRatio: 3, //最大比例
-        title: [4, image => `${image.alt}`],
-        viewed: function() {
-            //设置相对比例0.6=展示宽度/图片实际宽度
-            this.viewer.zoomTo(0.28).move(0, -6.2);
-        }
-    };
+    constructor(props) {
+        super(props);
+        this.options = {
+            inline: true, //内联模式
+            // button: false,//右上角全屏
+            navbar: false,
+            // title: false,
+            toolbar: {
+                //工具栏
+                zoomIn: 1,
+                zoomOut: 1,
+                oneToOne: 1,
+                reset: 1,
+                prev: 1,
+                play: 0,
+                next: 1,
+                rotateLeft: 0,
+                rotateRight: 0,
+                flipHorizontal: 0,
+                flipVertical: 0,
+                nextNode: this.next,
+                prevNode: this.previous
+            },
+            tooltip: false, //放大或缩小时显示具有图像比率（百分比）的工具提示
+            rotatable: false, //旋转图像
+            fullscreen: true, //全屏
+            transition: false, //过渡
+            backdrop: true, //控制背景
+            minWidth: 600,
+            zoomRatio: 0.1, //通过旋转鼠标缩放图像时定义比率
+            minZoomRatio: 0.1, //最小比例
+            maxZoomRatio: 3, //最大比例
+            title: [4, image => `${image.alt}`],
+            viewed: function() {
+                //设置相对比例0.6=展示宽度/图片实际宽度
+                this.viewer.zoomTo(0.28).move(0, -6.2);
+            }
+        };
+    }
 
     componentDidMount() {
         this.addListener();
@@ -97,6 +103,33 @@ class PictureShowView extends React.Component {
                 <img src={`${completeSecendUrl(url, activeTask)}`} />
             </li>
         );
+    };
+
+    next = () => {
+        const { PictureShowStore } = this.props;
+        let idx = PictureShowStore.getIdx() + 1;
+        this.jumpToPoint(idx);
+    };
+
+    previous = () => {
+        const { PictureShowStore } = this.props;
+        let idx = PictureShowStore.getIdx() - 1;
+        this.jumpToPoint(idx);
+    };
+
+    jumpToPoint = idx => {
+        const { PictureShowStore, DataLayerStore } = this.props;
+        window.traceLayer.getPoint(idx, item => {
+            window.traceLayer.unselect();
+            window.traceLayer.select(idx);
+            window.map.look({
+                x: item.properties.x,
+                y: item.properties.y,
+                z: item.properties.z
+            });
+            PictureShowStore.setPicData(item);
+        });
+        DataLayerStore.exitEdit();
     };
 }
 export default PictureShowView;
