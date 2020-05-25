942d224bc5ec7145f62781337ca7716441af5f1a
包星星 <743697070@qq.com>
2020-05-25 17:19:54 +0800

    要素形状编辑类型操作不更新关联关系渲染

--- src/utils/decorator.js
+++ src/utils/decorator.js
@@ -11,6 +11,7 @@ import AdEmitter from 'src/models/event';
  * @property {Object | String} option.operate 操作类型，为字符串或图层映射对象
  * @property {Boolean} option.onlyRun 为true时只运行包装函数并记录可能的错误日志，默认false
  * @property {Boolean} option.skipHistory 为true时跳过历史记录，默认false
+ * * @property {Boolean} option.skipRenderMode 为true时跳过渲染模式重绘，默认false
  * @returns {Object} 被打断/合并线要素的lines相关信息
  */
 export const logDecorator = option => {
@@ -18,7 +19,7 @@ export const logDecorator = option => {
         const fn = descriptor.value;
         descriptor.value = async function () {
             var log;
-            let { operate, onlyRun, skipHistory } = option;
+            let { operate, onlyRun, skipHistory, skipRenderMode } = option;
             if (typeof operate === 'object') {
                 let layerName = DataLayerStore.getEditLayer().layerName;
                 operate = operate[layerName];
@@ -29,8 +30,10 @@ export const logDecorator = option => {
                 if (!skipHistory) {
                     OperateHistoryStore.add(history);
                 }
-                // 更新关联关系渲染
-                RenderModeStore.updateRels(history);
+                if (!skipRenderMode) {
+                    // 更新关联关系渲染
+                    RenderModeStore.updateRels(history);
+                }
                 // 刷新属性列表
                 AdEmitter.emit('fetchViewAttributeData');
                 log = {
