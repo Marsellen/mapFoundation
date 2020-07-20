c3096fcabf3bce30fa4f837d50e3c9f915a88af5
vivian <wei.lu@ecarx.com.cn>
2020-07-20 19:37:25 +0800

    修改版本号v1.0.22.2

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export default {
     title: 'Ecarx React App',
-    version: 'v1.0.22',
+    version: 'v1.0.22.2',
     urlConfig: {
         point_clouds: 'cloud.js',
         track: 'track.json',
