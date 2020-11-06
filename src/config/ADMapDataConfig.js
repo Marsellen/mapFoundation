df37d2b0baf6c6a3563783af249a5048f436ea7d
包星星 <743697070@qq.com>
2020-11-06 16:46:50 +0800

    交通标志牌默认属性

--- src/config/ADMapDataConfig.js
+++ src/config/ADMapDataConfig.js
@@ -669,7 +669,7 @@ export const DEFAULT_PROPERTIES_MAP = {
     },
     AD_TrafficLight: {},
     AD_TrafficSign: {
-        SIGN_STYLE: 0,
+        SIGN_STYLE: 1,
         OBJ_FUNC: [{ SIGN_TYPE: 0, CONT_TYPE: 0, CONT_VALUE: 0, TIMEDOM: '' }]
     },
     AD_LaneDivider: {
