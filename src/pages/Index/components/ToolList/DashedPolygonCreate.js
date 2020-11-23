0aa66dbd59abe1f9e0940f11e2f6ab48bf3e185a
ZKCD/WangLifang/王丽芳 <lifang.wang-zkcd@ecarx.com.cn>
2020-11-23 18:29:56 +0800

    虚线面构建与后台联调已经可视化联调

--- src/pages/Index/components/ToolList/DashedPolygonCreate.js
+++ src/pages/Index/components/ToolList/DashedPolygonCreate.js
@@ -4,39 +4,55 @@ import ToolIcon from 'src/components/ToolIcon';
 import { inject, observer } from 'mobx-react';
 import AdMessage from 'src/components/AdMessage';
 import AdInputNumber from 'src/components/Form/AdInputNumber';
+import { plgCreate } from 'src/utils/relCtrl/operateCtrl';
+import { logDecorator, editInputLimit } from 'src/utils/decorator';
 import 'less/components/tool-icon.less';
 import 'less/components/dashed-polygon-create.less';
 
 const { Option } = Select;
 
-const DASHED_TYPE = [
+const PLG_TYPE_GROUP = [
     {
         label: '单虚线',
-        value: 1
+        value: 1002
     },
     {
         label: '双虚线',
-        value: 2
+        value: 1004
     },
     {
         label: '左实右虚线',
-        value: 3
+        value: 1005
     },
     {
         label: '左虚右实线',
-        value: 4
+        value: 1006
     }
 ];
 
 @inject('DataLayerStore')
+@inject('AttributeStore')
 @observer
 class DashedPolygonCreate extends React.Component {
     constructor() {
         super();
         this.state = {
+            step: 0,
             message: '',
-            msgVisible: false
-        }
+            msgVisible: false,
+            PLG_WIDTH: 0.15,
+            PLG_TYPE: 1002
+        };
+        this.result = [];
+    }
+
+    componentDidMount() {
+        const { DataLayerStore } = this.props;
+        DataLayerStore.setDashedPolygonCreateCallback(this.dashedPolygonCreateCallback);
+    }
+
+    componentWillUnmount() {
+        this.removeEventListener();
     }
     render() {
         const { DataLayerStore } = this.props;
@@ -67,6 +83,7 @@ class DashedPolygonCreate extends React.Component {
     }
 
     _renderModal = () => {
+        const { PLG_WIDTH, PLG_TYPE } = this.state;
         return (
             <div>
                 <div className="modal-flex">
@@ -74,18 +91,19 @@ class DashedPolygonCreate extends React.Component {
                     <AdInputNumber 
                         className="modal-content"
                         type="number"
-                        defaultValue={0.15}
+                        value={PLG_WIDTH}
                         max={1}
                         min={0.01}
                         step={0.01}
+                        onChange={(val) => { this.setState({ PLG_WIDTH: val }) }}
                      />
                      <span>m</span>
                 </div>
                 <div className="modal-flex">
                     <span>虚线面类型</span>
-                    <Select className="modal-content">
+                    <Select className="modal-content" value={PLG_TYPE} onChange={(val) => { this.setState({ PLG_TYPE: val }) }}>
                         {
-                            DASHED_TYPE.map((item, index) => (
+                            PLG_TYPE_GROUP.map((item, index) => (
                                 <Option key={index} value={item.value}>{item.label}</Option>
                             ))
                         }
@@ -95,13 +113,94 @@ class DashedPolygonCreate extends React.Component {
         )
     }
 
-    action = () => {
+    addEventListener = () => {
+        document.addEventListener('keyup', this.shiftCallback);
+    }
+
+    shiftCallback = event => {
         const { DataLayerStore } = this.props;
+        if (event.key !== 'Shift' || DataLayerStore.editType !== 'dashed_polygon_create') return;
+        try {
+            this.check();
+            this.useSdk();
+        } catch (e) {
+            this.setState({
+                message: e.message
+            });
+            setTimeout(() => {
+                this.setState({
+                    message: '选择一根车道线，按shift进入下一步'
+                })
+            }, 1000);
+        }
+    }
+
+    removeEventListener = () => {
+        document.removeEventListener('keyup', this.shiftCallback);
+    }
+
+    check = () => {
+        if (!this.result || this.result.length === 0) {
+            throw new Error('未选择要素！');
+        }
+        if (this.result.length != 1) {
+            throw new Error('只能选择一条车道线！')
+        }
+    }
+
+    useSdk = () => {
+        const { DataLayerStore } = this.props;
+        DataLayerStore.dashedPolygonCreate(1);
+        this.removeEventListener();
+        this.setState({
+            step: 1,
+            message: '在线上选择3个点，分别为虚线的实部起点、实部终点、虚部终点，点击右键完成构建',
+            msgVisible: true
+        });
+    }
+
+    dashedPolygonCreateCallback = async (result, event) => {
+        const { step } = this.state;
+        if (event.button !== 2) {
+            this.result = result;
+        }
+        if (step == 1) {
+            this.handerDashedPolygon(result, event);
+            this.setState({
+                step: 0,
+                msgVisible: false,
+                message: ''
+            })
+        }
+    }
+
+    @editInputLimit({ editType: 'dashed_polygon_create' })
+    @logDecorator({ operate: '虚线面构建' })
+    async handerDashedPolygon(pointData, event) {
+        if (event.button !== 2) return;
+        try {
+            const { PLG_WIDTH, PLG_TYPE } = this.state;
+            let LOOP_SIZE = [pointData[0].data.geometry.coordinates, pointData[1].data.geometry.coordinates, pointData[2].data.geometry.coordinates];
+            let history = await plgCreate(this.result, LOOP_SIZE, PLG_WIDTH, PLG_TYPE);
+            return history;
+        } catch (err) {
+            console.log(err);
+            this.removeEventListener();
+            throw err;
+        }
+    }
+
+    action = () => {
+        const { DataLayerStore, AttributeStore } = this.props;
         if (DataLayerStore.editType == 'dashed_polygon_create') return;
+        this.result = [];
+        AttributeStore.hideRelFeatures();
+        this.addEventListener();
         DataLayerStore.dashedPolygonCreate();
         this.setState({
+            step: 0,
             msgVisible: true,
-            message: '选择一根车道线 或 几何层：车道线线要素，按shift进入下一步'
+            message: '选择一根车道线，按shift进入下一步'
         });
     }
 }
