ad104d15f67a3b06cd1ea605b38104756ac91f6e
Wang-zkcdLifang/中科创达-王丽芳 <lifang.wang-zkcd@ecarx.com.cn>
2021-04-12 15:08:27 +0800

    日志查询下载功能

--- src/pages/Search/component/LogList.js
+++ src/pages/Search/component/LogList.js
@@ -1,11 +1,11 @@
 import React, { useContext } from 'react';
-import { ConfigProvider, Table, Divider } from 'antd';
+import { ConfigProvider, Table, Divider, Icon } from 'antd';
 import moment from 'moment';
 import zh_CN from 'antd/es/locale/zh_CN';
 import { TASK_PROCESS_NAME } from 'src/config/TaskConfig';
 import { LogStore } from 'src/pages/Search/store/LogStore';
 import LogModal from 'src/pages/Search/component/LogModal';
-import { getLogList } from 'src/pages/Search/request/LogRequest';
+import { getLogList, getLogDetail } from 'src/pages/Search/request/LogRequest';
 
 function LogList() {
     const { state = {}, dispatch, form } = useContext(LogStore);
@@ -29,6 +29,30 @@ function LogList() {
             });
             dispatch({ type: 'setLogList', logListData });
         });
+    };
+
+    const handleDetailData = async (record, label, type) => {
+        const logDetailData = await getLogDetail({
+            userName: record.userName,
+            taskId: record.taskId,
+            saveTime: record.saveTime,
+            dataType: type
+        });
+        downloadFileByBlob(record, logDetailData, label);
+    };
+
+    const downloadFileByBlob = (record, logDetailData, label) => {
+        const detailJsonIndex = `${record.userName}:${record.taskId}:${record.saveTime}`;
+        const totalData = JSON.stringify(logDetailData?.[detailJsonIndex], null, 4);
+        const blobContent = new Blob([totalData], { type: 'application/json' });
+        const blobUrl = window.URL.createObjectURL(blobContent);
+        const eleLink = document.createElement('a');
+        eleLink.download = `${label}.json`;
+        eleLink.style.display = 'none';
+        eleLink.href = blobUrl;
+        document.body.appendChild(eleLink);
+        eleLink.click();
+        document.body.removeChild(eleLink);
     };
 
     const columns = [
@@ -65,15 +89,26 @@ function LogList() {
                 return (
                     <div>
                         <LogModal type="incrementData" label="操作记录" record={record} />
+                        {_renderDownload(record, '操作记录', 'incrementData')}
                         <Divider type="vertical" />
                         <LogModal type="totalData" label="数据快照" record={record} />
+                        {_renderDownload(record, '数据快照', 'totalData')}
                         <Divider type="vertical" />
                         <LogModal type="all" label="所有数据" record={record} />
+                        {_renderDownload(record, '所有数据', 'all')}
                     </div>
                 );
             }
         }
     ];
+
+    const _renderDownload = (record, label, type) => {
+        return (
+            <a onClick={() => handleDetailData(record, label, type)}>
+                <Icon type="download" />
+            </a>
+        );
+    };
 
     return (
         <div className="log-list">
