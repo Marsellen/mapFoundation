fd52c1a5f2c94c4be52725cf1334779286de994c
linhai.zhu <linhai.zhu@ecarx.com.cn>
2021-08-04 14:04:21 +0800

    版本号修改

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export default {
     title: 'Ecarx React App',
-    version: 'SVC013_v02.01.01',
+    version: 'SVC013_v02.02.01',
     urlConfig: {
         point_clouds: 'cloud.js',
         track: 'track.json',
