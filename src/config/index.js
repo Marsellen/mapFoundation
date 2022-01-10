f9abf94756f524538cf45ba202648f841f5f38c9
lifang.wang <lifang.wang@ecarxgroup.com>
2022-01-10 18:37:24 +0800

    修改版本号

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export default {
     title: 'Ecarx React App',
-    version: 'SVC013_v03.06.2',
+    version: 'SVC013_v4.1.1.1',
     urlConfig: {
         point_clouds: 'cloud.js',
         track: 'track.json',
