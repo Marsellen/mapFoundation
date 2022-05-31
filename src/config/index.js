637723e2216f11223bc9919536305d96fee3f229
zhichao.wang <zhichao.wang@ecarxgroup.com>
2022-05-31 17:21:04 +0800

    fix version

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export default {
     title: 'Ecarx React App',
-    version: 'SVC013_v4.2.3.1',
+    version: 'SVC013_v4.2.4.1',
     urlConfig: {
         point_clouds: 'cloud.js',
         track: 'track.json',
