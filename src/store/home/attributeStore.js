7d7f32e80cd21ddab3543ae6795f21e5dbd39b3e
chiyanchao <yanchao.chi@ecarxgroup.com>
2022-08-05 11:29:48 +0800

    优化代码及修改周边title加载问题

--- src/store/home/attributeStore.js
+++ src/store/home/attributeStore.js
@@ -375,8 +375,7 @@ class AttributeStore {
             let MainFId = MainKey.key;
             properties = properties || this.model.data.properties;
             // 更新标记
-            if (key === 'AD_Lane_Speed') {
-                debugger
+            if (key === 'AD_Lane_Speed') { 
                 if (value?.UPD_STAT) {
                     let UPD_STAT = JSON.parse(value.UPD_STAT);
                     UPD_STAT.PRECOMPLIER_STAT = 'MAN';
