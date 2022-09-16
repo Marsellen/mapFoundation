cecc6627cf4ef697f472573ea151f27d07447443
zhichao.wang <zhichao.wang@ecarxgroup.com>
2022-09-16 11:15:30 +0800

    新增启动和最近样式

--- src/page/board.js
+++ src/page/board.js
@@ -6,6 +6,7 @@ import HomeVisiteHistory from 'src/util/visiteHistory/homeVisiteHistory';
 import LoginVisiteHistory from 'src/util/visiteHistory/loginVisiteHistory';
 import { getAuthentication } from 'src/util/session';
 import logo from 'src/asset/img/logo-x.svg';
+import { Col, Button, Row, Card, Icon } from 'antd';
 
 @withRouter
 class Board extends React.Component {
@@ -20,13 +21,73 @@ class Board extends React.Component {
                         <span className="name">Ultron</span>
                     </div>
                 </div>
-                <div className="board-content">新建项目 导入项目 浏览历史</div>
+                <div className="board-content">
+                    <Row gutter={[16, 60]}>
+                        <Col className="board-gutter-row" span={12}>
+                            <div>
+                                <h2>启动</h2>
+                                <p>
+                                    <Button type="link">
+                                        <Icon type="file-add" />
+                                        新建项目...
+                                    </Button>
+                                </p>
+                                <p>
+                                    <Button type="link" onClick={this.handleClickOpen}>
+                                        <Icon type="folder-open" />
+                                        打开...
+                                    </Button>
+                                </p>
+                            </div>
+                        </Col>
+                        <Col className="board-gutter-row" span={12}>
+                            <div></div>
+                        </Col>
+                    </Row>
+                    <Row gutter={16}>
+                        <Col className="board-gutter-row" span={12}>
+                            <div>
+                                <h2>最近</h2>
+                                <div className="site-card-wrapper">
+                                    <Row gutter={16}>
+                                        <Col span={8}>
+                                            <Card title="2022-09-16" bordered={false}>
+                                                Card content
+                                            </Card>
+                                        </Col>
+                                        <Col span={8}>
+                                            <Card title="2022-09-16" bordered={false}>
+                                                Card content
+                                            </Card>
+                                        </Col>
+                                        <Col span={8}>
+                                            <Card title="2022-09-16" bordered={false}>
+                                                Card content
+                                            </Card>
+                                        </Col>
+                                    </Row>
+                                </div>
+                            </div>
+                        </Col>
+                        <Col className="board-gutter-row" span={12}>
+                            <div></div>
+                        </Col>
+                    </Row>
+                </div>
                 <div className="board-mask"></div>
             </div>
         );
     }
 
     handleClick = () => {
+        HomeVisiteHistory.clearVisitedHistory();
+        LoginVisiteHistory.clearVisitedHistory();
+    };
+
+    handleClickOpen = () => {
+        // 导入数据 成功后 跳转到“/”
+
+        this.props.history.push('/');
         HomeVisiteHistory.clearVisitedHistory();
         LoginVisiteHistory.clearVisitedHistory();
     };
